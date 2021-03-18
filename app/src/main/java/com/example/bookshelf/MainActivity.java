e29131a3e86af200c74dc49d6f33461affe515ec
karl <karl.morris@temple.edu>
2021-03-18 10:19:59 -0400

    Initial commit

---
+++ app/src/main/java/com/example/bookshelf/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.bookshelf;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements BookListFragment.BookSelectedInterface {
+
+    FragmentManager fm;
+
+    boolean twoPane;
+    BookDetailsFragment bookDetailsFragment;
+    Book selectedBook;
+    private final String KEY_SELECTED_BOOK = "selectedBook";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Fetch selected book if there was one
+        if (savedInstanceState != null)
+            selectedBook = savedInstanceState.getParcelable(KEY_SELECTED_BOOK);
+
+        twoPane = findViewById(R.id.container2) != null;
+
+        fm = getSupportFragmentManager();
+
+        Fragment fragment1;
+        fragment1 = fm.findFragmentById(R.id.container1);
+
+
+        // At this point, I only want to have BookListFragment be displayed in container_1
+        if (fragment1 instanceof BookDetailsFragment) {
+            fm.popBackStack();
+        } else if (!(fragment1 instanceof BookListFragment))
+            fm.beginTransaction()
+                    .add(R.id.container1, BookListFragment.newInstance(getTestBooks()))
+            .commit();
+
+        /*
+        If we have two containers available, load a single instance
+        of BookDetailsFragment to display all selected books
+         */
+        bookDetailsFragment = (selectedBook == null) ? new BookDetailsFragment() : BookDetailsFragment.newInstance(selectedBook);
+        if (twoPane) {
+            fm.beginTransaction()
+                    .replace(R.id.container2, bookDetailsFragment)
+                    .commit();
+        } else if (selectedBook != null) {
+            /*
+            If a book was selected, and we now have a single container, replace
+            BookListFragment with BookDetailsFragment, making the transaction reversible
+             */
+            fm.beginTransaction()
+                    .replace(R.id.container1, bookDetailsFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
+
+    }
+
+    /*
+    Generate an arbitrary list of "books" for testing
+     */
+    private BookList getTestBooks() {
+        BookList books = new BookList();
+        Book book;
+        for (int i = 1; i <= 10; i++) {
+            books.add(book = new Book("Book" + i, "Author" + i));
+        }
+        return books;
+    };
+
+    @Override
+    public void bookSelected(int index) {
+        //Store the selected book to use later if activity restarts
+        selectedBook = getTestBooks().get(index);
+
+        if (twoPane)
+            /*
+            Display selected book using previously attached fragment
+             */
+            bookDetailsFragment.displayBook(selectedBook);
+        else {
+            /*
+            Display book using new fragment
+             */
+            fm.beginTransaction()
+                    .replace(R.id.container1, BookDetailsFragment.newInstance(selectedBook))
+                    // Transaction is reversible
+                    .addToBackStack(null)
+                    .commit();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(KEY_SELECTED_BOOK, selectedBook);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // If the user hits the back button, clear the selected book
+        selectedBook = null;
+        super.onBackPressed();
+    }
+}
